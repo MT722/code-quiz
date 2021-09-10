da2a20ed127edbe49735e18bc61ed54c7486f3ff
MT722 <85648603+MT722@users.noreply.github.com>
2021-09-09 17:27:47 -0700

    constructed question 1 and answers

---
+++ script.js
@@ -0,0 +1,13 @@
+const quest1 = function(){
+    const question = "Commonly used datatypes do not include";
+    const ans1 = "strings"
+    const ans2 = "booleans"
+    const ans3 = "alerts"
+    const ans4 = "numbers"
+
+    
+
+
+
+    return null;
+}
