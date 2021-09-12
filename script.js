57ac9f299403d1a3acb2c82a3f69a098198be9a8
MT722 <85648603+MT722@users.noreply.github.com>
2021-09-12 12:52:05 -0700

    deleted tester files, changed questions in js, and moved start button element to outside of answers div

--- script.js
+++ script.js
@@ -8,44 +8,52 @@ var highScore = document.getElementById("view")
 var startQuiz = document.getElementById("start")
 var questions = [
     {
-        question = "Commonly used datatypes do not include",
-         ans1 = "strings",
-         ans2 = "booleans",
-         ans3 = "alerts",
-         ans4 = "numbers",
+        question : "Commonly used datatypes do not include",
+         ans1 : "strings",
+         ans2 : "booleans",
+         ans3 : "alerts",//right answer
+         ans4 : "numbers",
     },
     {
-        question = "Commonly used datatypes do not include",
-         ans1 = "strings",
-         ans2 = "booleans",
-         ans3 = "alerts",
-         ans4 = "numbers",
+        question : "What does CSS stand for?",
+         ans1 : "Citrus Saphron Spice",//answer
+         ans2 : "Course Salt Seasoning",
+         ans3 : "Canned Salted Sardines",
+         ans4 : "Cascading Style Sheets",
     },
     {
-        question = "Commonly used datatypes do not include",
-         ans1 = "strings",
-         ans2 = "booleans",
-         ans3 = "alerts",
-         ans4 = "numbers",
+        question : "What are the main laguages of web development?",
+         ans1 : "HTML, CSS, JavaScript",
+         ans2 : "Java",
+         ans3 : "C++",
+         ans4 : "Python",
     },
     {
-        question = "Commonly used datatypes do not include",
-         ans1 = "strings",
-         ans2 = "booleans",
-         ans3 = "alerts",
-         ans4 = "numbers",
+        question : "What does 'full stack' mean?",
+         ans1 : "A stack of pancakes",
+         ans2 : "Relating to front and back end applications",
+         ans3 : "A stack of books",
+         ans4 : "A stack of cups",
     },
     {
-        question = "Commonly used datatypes do not include",
-         ans1 = "strings",
-         ans2 = "booleans",
-         ans3 = "alerts",
-         ans4 = "numbers",
+        question : "",
+         ans1 : "strings",
+         ans2 : "booleans",
+         ans3 : "alerts",
+         ans4 : "numbers",
     },
 ]
+//start the quiz
 function start(){
 
+    console.log('hello world');
+    return 'hello world';
+}
+function ques1(){
+    
 }
 
 
-start.addEventListener('click', start());
+ startQuiz.addEventListener('click', start());
+
+console.log(start());
