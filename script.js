dd436f799d451b3ae70d7fd56b82d0f20992bc76
MT722 <85648603+MT722@users.noreply.github.com>
2021-09-16 12:32:18 -0700

    added responses to question functionality, changed tryagain button from class to id, modified starter function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let score = 0/5;
+let score = 0;
 var first = document.getElementById("ans1")
 var second = document.getElementById("ans2")
 var third = document.getElementById("ans3")
@@ -6,6 +6,12 @@ var fourth = document.getElementById("ans4")
 let questNum = 0;
 var highScore = document.getElementById("view")
 var start = document.getElementById("start")
+var question = document.querySelector(".question")
+var scores = document.querySelector(".scores")
+var input = document.querySelector(".input")
+var timer = 0;
+var again = document.getElementById('tryAgain');
+var card = document.querySelector('.card');
 var questions = [
     {
         question : "Commonly used datatypes do not include",
@@ -16,10 +22,10 @@ var questions = [
     },
     {
         question : "What does CSS stand for?",
-         ans1 : "Citrus Saphron Spice",//answer
+         ans1 : "Citrus Saphron Spice",
          ans2 : "Course Salt Seasoning",
          ans3 : "Canned Salted Sardines",
-         ans4 : "Cascading Style Sheets",
+         ans4 : "Cascading Style Sheets",//answer
     },
     {
         question : "What are the main laguages of web development?",
@@ -37,23 +43,124 @@ var questions = [
     }
     
 ]
-//start the quiz
+// question.style.display = 'none';
+scores.style.display = 'none';
+input.style.display = 'none';
+//qustion functions
+
+function ques1(){
+    
+    first.textContent = (questions[0].ans1);
+    first.addEventListener("click", function(){
+        ques2();
+    });
+    second.textContent = (questions[0].ans2);
+    second.addEventListener("click", function(){
+        ques2();
+    });
+    third.textContent = (questions[0].ans3);
+    third.addEventListener("click", function(){
+        score += 2;
+        console.log(score);
+        ques2();
+    });
+    fourth.textContent = (questions[0].ans4);
+    fourth.addEventListener("click", function(){
+        ques2();
+    });
+    
+    
+}
+
+function ques2(){
+    first.textContent = (questions[1].ans1);
+    first.addEventListener("click", function(){
+        ques3();
+        console.log(score);
+    });
+    second.textContent = (questions[1].ans2);
+    second.addEventListener("click", function(){
+        ques3();
+        console.log(score);
+    });
+    third.textContent = (questions[1].ans3);
+    third.addEventListener("click", function(){
+        ques3();
+        console.log(score);
+    });
+    fourth.textContent = (questions[1].ans4);
+    fourth.addEventListener("click", function(){
+        score += 2;
+        console.log(score);
+        ques3();
+    });
+    
+    
+}
+
+function ques3(){
+    first.textContent = (questions[2].ans1);
+    first.addEventListener("click", function(){
+        score += 2;
+        console.log(score);
+        ques4();
+    });
+    second.textContent = (questions[2].ans2);
+    second.addEventListener("click", function(){
+        ques4();
+        console.log(score);
+    });
+    third.textContent = (questions[2].ans3);
+    third.addEventListener("click", function(){
+        ques4();
+        console.log(score);
+    });
+    fourth.textContent = (questions[2].ans4);
+    fourth.addEventListener("click", function(){
+        ques4();
+        console.log(score);
+    });
+    
+    
+}
+
+function ques4(){
+    first.textContent = (questions[3].ans1);
+    first.addEventListener("click", function(){
+        console.log(score);
+    });
+    second.textContent = (questions[3].ans2);
+    second.addEventListener("click", function(){
+        score += 2;
+        console.log(score);
+        card.style.display = 'none';
+    });
+    third.textContent = (questions[3].ans3);
+    third.addEventListener("click", function(){
+        console.log(score);
+    });
+    fourth.textContent = (questions[3].ans4);
+    fourth.addEventListener("click", function(){
+        console.log(score);
+    });
+    
+    
+}
 
 function starter(){
-    first.textContent = (questions[3].ans1);
     console.log('hello world');
-   
+    ques1();
+    score = 0;
 }
 start.addEventListener("click", function(){
     starter();
+    start.style.display = "none";
 });
-function ques1(){
-    first.textContent = (questions[0].ans1);
-    second.textContent = (questions[1].ans2);
-    third.textContent = (questions[2].ans3);
-    fourth.textContent = (questions[3].ans4);
-}
 
 
+again.addEventListener("click", function(){
+    
+    
+});
 
  
